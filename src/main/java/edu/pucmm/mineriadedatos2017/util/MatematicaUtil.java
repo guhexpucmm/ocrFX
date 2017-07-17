c36eaa7aaf56dab0c5e45cda9d3371164515d784
Gustavo Jose Henríquez Contreras <gustavojoseh@gmail.com>
2017-07-17 17:30:44 -0400

    Clase borrada.

--- src/main/java/edu/pucmm/mineriadedatos2017/util/MatematicaUtil.java
+++
@@ -1,7 +0,0 @@
-package edu.pucmm.mineriadedatos2017.util;
-
-public class MatematicaUtil {
-    public static double valorSigmoid(Double arg) {
-        return (1 / (1 + Math.exp(-arg)));
-    }
-}
