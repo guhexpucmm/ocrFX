887f2286b5c956423ebd1c294011b013d03011fa
Jeanlo <jeanlotejeda0101@hotmail.com>
2017-07-17 18:05:08 -0400

    minusculas agregadas.

--- src/main/java/edu/pucmm/mineriadedatos2017/data/LeerEscribirArchivos.java
+++ src/main/java/edu/pucmm/mineriadedatos2017/data/LeerEscribirArchivos.java
@@ -50,7 +50,7 @@ public class LeerEscribirArchivos {
             pa.write("@attribute fila" + i + " string\n");
         }
 
-        pa.write("@attribute class {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}\n\n");
+        pa.write("@attribute class {A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,W,X,Y,Z, a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z}\n\n");
         pa.write("@data\n");
     }
 }
